b5a8f1897d250f81fe61d6807584d49bf12d1259
James <ilsh1022@gmail.com>
2012-04-20 15:13:59 +0800

    Handling Forms with webapp2

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,14 +1,27 @@
 import webapp2
+import cgi
 from google.appengine.api import users
 
 class MainPage(webapp2.RequestHandler):
       def get(self):
-           user = users.get_current_user()
-           if user:
-                 self.response.headers['Content-Type'] = 'text/plain'
-                 self.response.out.write('Hello, '+ user.nickname())
-           else :
-                 self.redirect(users.create_login_url(self.request.uri))
+          self.response.out.write("""
+            <html>
+              <body>
+                <form action="/sign" method="post">
+                  <div><textarea name="content" row="3" cols="60"></textarea></div>
+                  <div><input type="submit" value="Sign Guestbook"></div>
+                </form>
+               </body>
+            </html>""")
 
-app = webapp2.WSGIApplication([('/', MainPage)],
+
+class Guestbook(webapp2.RequestHandler):
+            def post(self):
+                self.response.out.write('<html><body>You wrote:<pre>')
+                self.response.out.write(cgi.escape(self.request.get('content')))
+                self.response.out.write('</pre></body></html>')
+             
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/sign',Guestbook)],
                                 debug = True)
